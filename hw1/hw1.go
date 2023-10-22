b8fca8abeb839dfe77991da3664b122671341731
Guvanchojamov <guvanchhojamov@gmail.com>
2023-10-22 20:43:43 +0500

    Hw number one

--- hw1/hw1.go
+++ hw1/hw1.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/beevik/ntp"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	l := log.New(os.Stderr, "hw1- ", log.Flags())
+	if time, err := ntp.Time("0.beevik-ntp.pool.ntp.org"); err != nil {
+		l.Fatalln(err)
+	} else {
+		fmt.Print(time.Format("Monday January 02 15:05:20 -0700 MST 2006"))
+	}
 }
